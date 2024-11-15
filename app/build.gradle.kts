f240467d83667acae9c5ac80ed9c4ffb14ab6303
IPseudoNemoI <nemoarkoni@mail.ru>
2024-11-15 23:55:08 +0700

    Prepare project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -42,7 +45,4 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
